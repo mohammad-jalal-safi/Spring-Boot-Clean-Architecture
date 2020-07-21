5df6b8342ade0772c133676d2e153bd7160adf7d
mohammad-jalal-safi <47706591+mohammad-jalal-safi@users.noreply.github.com>
2020-07-21 18:45:30 +0430

    Base Structure

---
+++ application/src/main/java/com/clean/application/Application.java
@@ -0,0 +1,12 @@
+package com.clean.application;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan("com.clean.persistence")
+public class Application {
+
+}
